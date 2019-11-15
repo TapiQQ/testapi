61db2f281be25d7e9a642c5a343f0d41ab7329f3
tsarkka <tapio.sarkka@nokia.com>
2019-11-15 15:14:05 +0200

    Improved the healthcheck function ***UNTESTED***

--- testapi/handler.py
+++ testapi/handler.py
@@ -3,14 +3,56 @@ def handle(req):
 	This will be a health check function for all uMEC API endpoints
 
 	Arg: req is a placeholder, doesn't do anything
+	***TODO: how to call handler function in OpenFaas without arguments***
 
-	returns status code of one API endpoint (detection)
-
+	Returns status of all uMEC APIs deployed in Junction challenge:
+	
+	uMEC MEC011 Application Enablement API: http://micromec.org:32000
+	uMEC Sensors API: http://micromec.org:32001
+	uMEC Detection API: http://micromec.org:32002
+	uMEC Lights API: http://micromec.org:32003
+	uMEC Camera API: http://micromec.org:32004
+	
+	Healthcheck: Considers endpoint alive if response status code is 2XX, else considers it dead.
+	
+	***UNTESTED in the Openfaas Platform, only in regular Python Interpreter***
+	
+	Ret: Generic success message
+	
     """
 
     import requests
+	
+	response = requests.get("http://micromec.org:32000").status_code
+	if response in range(200,226):
+		print("uMEC MEC011 Application Enablement API is alive! Response:" + str(response))
+	else:
+		print("uMEC MEC011 Application Enablement API is dead! Response:" + str(response))
 
-    response = requests.get("http://192.168.20.106:32001/v1/models/")
-
-    return response.status_code
+	response = requests.get("http://micromec.org:32001").status_code
+	if response in range(200,226):
+		print("uMEC Sensors API is alive! Response:" + str(response))
+	else:
+		print("uMEC Sensors API is dead! Response:" + str(response))
+	
+	response = requests.get("http://micromec.org:32002").status_code
+	if response in range(200,226):
+		print("uMEC Detection API is alive! Response:" + str(response))
+	else:
+		print("uMEC Detection API is dead! Response:" + str(response))
+	
+	response = requests.get("http://micromec.org:32003").status_code
+	if response in range(200,226):
+		print("uMEC Lights API is alive! Response:" + str(response))
+	else:
+		print("uMEC Lights API is dead! Response:" + str(response))
+	
+	response = requests.get("http://micromec.org:32004").status_code
+	if response in range(200,226):
+		print("uMEC Camera API is alive! Response:" + str(response))
+	else:
+		print("uMEC Camera API is dead! Response:" + str(response))
+	
+	
+    return "Function finished successfully"
 
